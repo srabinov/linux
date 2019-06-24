968e51cfddd86bffd4fc7d0ccf5ad753a631df4e
Shamir Rabinovitch <shamir.rabinovitch@oracle.com>
2019-06-24 15:32:19 +0300

    IB/uverbs: Add PD import verb
    This verb import pd from given shared context to the current context.
    New uobject is created in the current context and the shared ib_pd
    refcnt is incremented.
    
    Signed-off-by: Shamir Rabinovitch <shamir.rabinovitch@oracle.com>

--- include/rdma/ib_verbs.h
+++ include/rdma/ib_verbs.h
@@ -2617,6 +2617,9 @@ struct ib_device_ops {
 	int (*iw_create_listen)(struct iw_cm_id *cm_id, int backlog);
 	int (*iw_destroy_listen)(struct iw_cm_id *cm_id);
 
+	/* Object sharing callbacks */
+	clone_callback(ib_pd);
+
 	DECLARE_RDMA_OBJ_SIZE(ib_ah);
 	DECLARE_RDMA_OBJ_SIZE(ib_pd);
 	DECLARE_RDMA_OBJ_SIZE(ib_srq);
@@ -2629,6 +2632,9 @@ struct ib_device_ops {
 	{ \
 		return 0; \
 	}
+
+/* Shared IB HW object support */
+trivial_clone_callback(ib_pd);
 
 struct ib_core_device {
 	/* device must be the first element in structure until,
